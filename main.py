19cb81c65b6b29344db5cac4200d0c71bd04a95e
Majid Ansari <MAJID_ANSARI24@manavrachna.net>
2025-06-04 20:04:28 +0530

    Improve UI

--- main.py
+++ main.py
@@ -143,5 +143,10 @@ def index():
 
             return redirect('/')
     return render_template('/index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
