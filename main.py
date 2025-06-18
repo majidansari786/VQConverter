463df7835461ee35fb801b372fd59dccb7d70562
Majid Ansari <MAJID_ANSARI24@manavrachna.net>
2025-06-18 22:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -290,4 +290,4 @@ def history():
     return render_template('/history.html', encodes=encodes,BUCKET_NAME=BUCKET_NAME)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
